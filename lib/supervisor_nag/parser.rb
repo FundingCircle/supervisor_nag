fafc31de8b51a8cd413e4841d7cb6387f2ba791f
jspc <james@zero-internet.org.uk>
2014-06-26 18:19:04 +0100

    Do something

--- lib/supervisor_nag/parser.rb
+++ lib/supervisor_nag/parser.rb
@@ -1,8 +1,10 @@
 module SupervisorNag
+  class DaFuq < RuntimeError; end
+
   module Parser
     def self.parse app
-      apps = []
       supervisorctl = `supervisorctl status`
+
       supervisorctl.split("\n").each do |line|
         line_arr = line.split
 
@@ -12,9 +14,10 @@ module SupervisorNag
         state = line_arr[1]
         since = line_arr[1] == 'STOPPED' ? line_arr[2..-1].join(' ') : line_arr[5..-1]
 
+        apps ||= []
         apps << SupervisorNag::Application.new(name, state, since)
       end
-      apps
+      apps or raise DaFuq
     end
   end
 end
