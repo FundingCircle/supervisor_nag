1b88c92fd71b7c81a9942b51ba9c2c26ff891aac
root <root@mickeymania.fcuat.co.uk>
2014-06-26 17:41:19 +0000

    Fix busteds

--- lib/supervisor_nag/application.rb
+++ lib/supervisor_nag/application.rb
@@ -2,7 +2,7 @@ module SupervisorNag
   class Application
     attr_reader :name, :state, :since
 
-    def intialize name, state, since
+    def initialize name, state, since
       @name  = name
       @state = state.downcase.to_sym
       @since = since
--- lib/supervisor_nag/parser.rb
+++ lib/supervisor_nag/parser.rb
@@ -4,20 +4,22 @@ module SupervisorNag
   module Parser
     def self.parse app
       supervisorctl = `supervisorctl status`
+      apps = nil
 
       supervisorctl.split("\n").each do |line|
-        line_arr = line.split
+        line_arr = line.strip.split
 
         name = line_arr[0]
         next unless name =~ /#{app}/
 
         state = line_arr[1]
-        since = line_arr[1] == 'STOPPED' ? line_arr[2..-1].join(' ') : line_arr[5..-1]
+        since = line_arr[1] == 'STOPPED' ? line_arr[2..-1].join(' ') : line_arr[5..-1].join(' ')
 
-        apps ||= []
+        apps = [] if apps.nil?
         apps << SupervisorNag::Application.new(name, state, since)
       end
-      apps or raise DaFuq
-    end
+      raise DaFuq if apps.nil?
+      return apps
+   end
   end
 end
