ae79dba68dd9a97f77306f340d83ae7be360b6d2
jspc <james@zero-internet.org.uk>
2014-06-26 18:01:08 +0100

    Tidy up parser

--- lib/supervisor_nag/parser.rb
+++ lib/supervisor_nag/parser.rb
@@ -1,18 +1,20 @@
 module SupervisorNag
   module Parser
-    def self.parse app=nil
+    def self.parse app
       apps = []
-      supervisorctl = `supervisorctl status #{app}`
+      supervisorctl = `supervisorctl status`
       supervisorctl.split("\n").each do |line|
-        line.split!
+        line_arr = line.split
 
-        name = line[0]
-        state = line[1]
-        since = line[1] == 'STOPPED' ? line[2..-1].join(' ') : line[5..-1]
+        name = line_arr[0]
+        next unless name =~ /#{app}/
 
-        app << SupervisorNag::Application.new(name, state, since)
+        state = line_arr[1]
+        since = line_arr[1] == 'STOPPED' ? line_arr[2..-1].join(' ') : line_arr[5..-1]
+
+        apps << SupervisorNag::Application.new(name, state, since)
       end
-      app
+      apps
     end
   end
 end
